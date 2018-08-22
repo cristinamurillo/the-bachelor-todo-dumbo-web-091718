da8bf49cdbdc318cd6196627f9dc098e8c7209ea
Cristina Murillo <cmurill1@jhu.edu>
2018-08-22 21:31:36 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,44 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner =""
+  data[season].each { |inner_hash|
+      inner_hash.each { |category, result|
+        if category == "status" && result == "Winner"
+          winner = inner_hash["name"] 
+        end
+      }
+  }
+  winner.split(" ")[0]
+  
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant = ""
+  data.each { |season, values|
+    values.each { |inner_hash|
+      inner_hash.each { |category, result|
+        if category == "occupation" && occupation == result
+          contestant = inner_hash["name"]
+        end  
+      }
+    }
+  }
+  contestant 
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0 
+  data.each { |season, values|
+    values.each { |inner_hash|
+      binding.pry 
+      if inner_hash["hometown"] == hometown 
+        counter+=1 
+      end
+    }
+  }
+  puts counter 
+  counter 
 end
 
 def get_occupation(data, hometown)
